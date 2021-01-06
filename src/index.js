198d6e0b8fc127d81535c967438b0753326fce3b
flis_ann <ann.flismark@gmail.com>
2021-01-06 16:05:49 +0100

    Adding code for rootReducer. Hello is displayed when running yarn start

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import configureStore from './state/store/configureStore'
 import { Provider } from "react-redux";
+import axios from 'axios'
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
-const store = configureStore
+const store = configureStore()
 window.store = store
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
+    <App />
     </Provider>,
   document.getElementById('root')
 );

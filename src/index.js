5ad1e594a3f3f8e01bf2588a4ac33dfeff68302a
flis_ann <ann.flismark@gmail.com>
2021-01-06 15:55:26 +0100

    Setting up redux. Creating first test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './state/store/configureStore'
+import { Provider } from "react-redux";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+const store = configureStore
+window.store = store
 
 ReactDOM.render(
+  <Provider store={store}>
     <App />,
+    </Provider>,
   document.getElementById('root')
 );
 

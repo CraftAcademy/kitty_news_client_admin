a2eb11fdfbd5eb2ed54ec686bc0b3ff3db19e80d
flis_ann <ann.flismark@gmail.com>
2021-01-06 16:18:58 +0100

    Creating test for article title, lead and body

--- cypress/integration/journalistCanCreateAnArticle.feature.js
+++ cypress/integration/journalistCanCreateAnArticle.feature.js
@@ -1,3 +1,18 @@
-describe('', () => {
-  
-})
+describe("Journalist can create an article", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/articles",
+      response: "fixture:successfully_create_article.json",
+    });
+  });
+  cy.visit("/");
+
+  it("is expected to ", () => {
+    cy.get("[data-cy='title-field']").type("Article Title");
+    cy.get("[data-cy='lead-field']").type("Article Lead");
+    cy.get("[data-cy='body-field']").type("Article Body");
+    cy.get("[data-cy='create-article-button']").click();
+  });
+});

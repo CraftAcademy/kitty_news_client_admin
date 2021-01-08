929a8790ca9c0f1247e2a7a9640446214046f7de
viktor <viktorkarlsson125@gmail.com>
2021-01-08 22:10:25 +0100

    hardkoding all category_id to 1 to check if i'm able to create an article with category_id. it pass

--- src/components/CreateArticleForm.jsx
+++ src/components/CreateArticleForm.jsx
@@ -7,7 +7,6 @@ import {
   Input,
   TextArea,
   Message,
-  Checkbox,
 } from "semantic-ui-react";
 
 import { useDispatch, useSelector } from "react-redux";
@@ -16,6 +15,10 @@ import { CreateArticle } from "../modules/CreateArticle";
 function CreateArticleForm() {
   const dispatch = useDispatch();
   const message = useSelector((state) => state.createArticleMessage);
+
+  // function updateInput(value) {
+  //   document.getElementById("sports").value = "sport"
+  // }
 
   return (
     <Container>
@@ -45,28 +48,47 @@ function CreateArticleForm() {
           name="body"
           placeholder="Body"
         />
-        <div>
-        <br />
-          <input type="checkbox" name="global_politics" id="global_politics" data-cy='categories-global-politics'/>
+          <br />
+          <input
+            type="checkbox"
+            name="category1"
+            value={0}
+            data-cy="categories-global-politics"
+          />
           <label for="global_politics">Global Politics</label>
           <br />
-          <input type="checkbox" name="sports" id="sports" data-cy='categories-sports'/>
+          <input
+            type="checkbox"
+            name="category2"
+            value={1}
+            data-cy="categories-sports"
+            // onChange={(value) => updateInput(value)}
+          />
           <label for="sports">Sports</label>
           <br />
-          <input type="checkbox" name="self_care" id="self_care" data-cy='categories-self-care'/>
+
+          {/* <input type="text" name="fieldname" id="fieldname" />
+          <input type="text" name="thingy" onchange="updateInput(value)" /> */}
+
+
+          <input
+            type="checkbox" name="self_care" id="self_care" data-cy="categories-self-care" />
           <label for="self_care">Self Care</label>
           <br />
-          <input type="checkbox" name="news" id="news" data-cy='categories-news'/>
+          <input type="checkbox" name="news" id="news" data-cy="categories-news" />
           <label for="news">News</label>
           <br />
-          <input type="checkbox" name="culture" id="culture" data-cy='categories-culture'/>
+          <input
+            type="checkbox"
+            name="culture"
+            id="culture"
+            data-cy="categories-culture"
+          />
           <label for="culture">Culture</label>
           <br />
-        </div>
 
-
-        <br/>
-        <br/>
+        <br />
+        <br />
         <Button
           data-cy="create-article-button"
           type="submit"
--- src/modules/CreateArticle.js
+++ src/modules/CreateArticle.js
@@ -4,14 +4,15 @@ const CreateArticle = {
   async create(event, dispatch) {
     event.preventDefault();
     try {
+      debugger
       let response = await axios.post("/articles", {
         article: {
           title: event.target.title.value,
           lead: event.target.lead.value,
           body: event.target.body.value,
+          category_id: event.target.category2.value
         },
       });
-
       dispatch({
         type: "SET_ARTICLE_MESSAGE",
         payload: response.data.message,

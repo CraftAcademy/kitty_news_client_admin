8200d462d1e1bd0f8926ad1b86fa33117ed857aa
Ehugo2000 <erik.hgsn@gmail.com>
2021-01-10 12:09:30 +0100

    Update URL in index with: axios.defaults.baseURL = http://localhost:3000/api.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = "http://localhost:3000/api";
 
 const store = configureStore();
 window.store = store;

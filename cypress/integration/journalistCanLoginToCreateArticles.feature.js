5dafdd808bc82ff8c8f6dd665002cd698a0c5af8
viktor <viktorkarlsson125@gmail.com>
2021-01-10 16:24:23 +0100

    creating login test and creating fixture file with journalist

---
+++ cypress/integration/journalistCanLoginToCreateArticles.feature.js
@@ -0,0 +1,77 @@
+describe("Journalist can login to create articles", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth/sign_in",
+      response: "fixture:journalist_can_login.json",
+      headers: {
+        uid: "journalist@mail.com",
+      },
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/auth/validate_token**",
+      response: "fixture:journalist_can_login.json",
+    });
+    cy.visit("/");
+  });
+
+  it("successfully with correct credentials", () => {
+    cy.get("[data-cy='login-form']").within(() => {
+      cy.get("[data-cy='email']").type("journalist@mail.com");
+      cy.get("[data-cy='password']").type("password");
+      cy.get("[data-cy='submit-btn']").contains("Submit").click();
+    });
+    cy.get("[data-cy='header-user-email']").should(
+      "contain",
+      "Logged in as journalist@mail.com"
+    );
+  });
+
+  it("unsuccessfully with wrong credentials", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth/sign_in",
+      status: "401",
+      response: {
+        errors: ["Invalid login credentials. Please try again."],
+        success: false,
+      },
+    });
+
+    cy.get("[data-cy='login-form']").within(() => {
+      cy.get("[data-cy='email']").type("journalist@mail.com");
+      cy.get("[data-cy='password']").type("wrongpassword");
+      cy.get("[data-cy='submit-btn']").contains("Submit").click();
+      cy.get("[data-cy='error-message']").contains(
+        "Invalid login credentials. Please try again."
+      );
+    });
+    cy.get("[data-cy='header-user-email']").contains("You're not logged in.");
+  });
+
+  it("sad path: unsuccessfully with right credentials but not an journalist", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth/sign_in",
+      response: "fixture:registered_user_can_not_log_in.json",
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/auth/validate_token**",
+      response: "fixture:registered_user_can_not_log_in.json",
+    });
+    cy.visit("/");
+
+    cy.get("[data-cy='login-form']").within(() => {
+      cy.get("[data-cy='email']").type("registered@user.com");
+      cy.get("[data-cy='password']").type("password");
+      cy.get("[data-cy='submit-btn']").contains("Submit").click();
+      cy.get("[data-cy='error-message']").contains(
+        "You are not authorized to be here"
+      );
+    });
+    cy.get("[data-cy='header-user-email']").contains("You're not logged in.");
+  });
+});

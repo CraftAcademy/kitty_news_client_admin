153adf406a034203731d29d0059b608e40d100cd
viktor <viktorkarlsson125@gmail.com>
2021-01-11 11:55:46 +0100

    fixing naming standards

--- cypress/integration/journalistCanCreateAnArticle.feature.js
+++ cypress/integration/journalistCanCreateAnArticle.feature.js
@@ -21,44 +21,28 @@ describe("Journalist can create an article", () => {
       cy.get("[data-cy='submit-btn']").contains("Submit").click();
     });
   });
-
-  it("is expected to successfully fill in ", () => {
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/articles",
-      response: { message: "Your article was successfully created" },
-    });
-    cy.get("[data-cy='article-form']").within(() => {
-      cy.get("[data-cy='title-field']").type("Article Title");
-      cy.get("[data-cy='lead-field']").type("Article Lead");
-      cy.get("[data-cy='body-field']").type("Article Body");
-      cy.get('[data-cy="categories-dropdown"]').select("Culture");
-      cy.get("[data-cy='create-article-button']").click();
-      cy.get("[data-cy='api-response-success-message']").should(
-        "contain",
-        "Your article was successfully created"
-      );
+  describe("successfully", () => {
+    it("when all fields are filled in ", () => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/articles",
+        response: { message: "Your article was successfully created" },
+      });
+      cy.get("[data-cy='article-form']").within(() => {
+        cy.get("[data-cy='title-field']").type("Article Title");
+        cy.get("[data-cy='lead-field']").type("Article Lead");
+        cy.get("[data-cy='body-field']").type("Article Body");
+        cy.get('[data-cy="categories-dropdown"]').select("Culture");
+        cy.get("[data-cy='create-article-button']").click();
+        cy.get("[data-cy='api-response-success-message']").should(
+          "contain",
+          "Your article was successfully created"
+        );
+      });
     });
   });
 
-  describe("Sad path: Journalist can not create an article", () => {
-    beforeEach(() => {
-      cy.server();
-      cy.route({
-        method: "POST",
-        url: "http://localhost:3000/api/auth/sign_in",
-        response: "fixture:journalist_can_login.json",
-        headers: {
-          uid: "journalist@mail.com",
-        },
-      });
-      cy.route({
-        method: "GET",
-        url: "http://localhost:3000/api/auth/validate_token**",
-        response: "fixture:journalist_can_login.json",
-      });
-    });
-
+  describe("unsuccessfully", () => {
     it("when title is not filled in ", () => {
       cy.route({
         method: "POST",
--- cypress/integration/journalistCanLoginAndSeeArticleForm.feature.js
+++ cypress/integration/journalistCanLoginAndSeeArticleForm.feature.js
@@ -16,64 +16,69 @@ describe("Journalist can login to see article form", () => {
     });
     cy.visit("/");
   });
-  it("successfully with correct credentials", () => {
-    cy.get("[data-cy='login-form']").within(() => {
-      cy.get("[data-cy='email']").type("journalist@mail.com");
-      cy.get("[data-cy='password']").type("password");
-      cy.get("[data-cy='submit-btn']").contains("Submit").click();
-    });
-    cy.get("[data-cy='header-user-email']").should(
-      "contain",
-      "Meow! Welcome back journalist@mail.com"
-    );
-    cy.get("[data-cy='article-form']").should("exist");
-  });
-  it("unsuccessfully with wrong credentials", () => {
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/auth/sign_in",
-      status: "401",
-      response: {
-        errors: ["Invalid login credentials. Please try again."],
-        success: false,
-      },
-    });
-    cy.get("[data-cy='login-form']").within(() => {
-      cy.get("[data-cy='email']").type("journalist@mail.com");
-      cy.get("[data-cy='password']").type("wrongpassword");
-      cy.get("[data-cy='submit-btn']").contains("Submit").click();
-      cy.get("[data-cy='error-message']").contains(
-        "Invalid login credentials. Please try again."
+  describe("successfully", () => {
+    it("with correct credentials", () => {
+      cy.get("[data-cy='login-form']").within(() => {
+        cy.get("[data-cy='email']").type("journalist@mail.com");
+        cy.get("[data-cy='password']").type("password");
+        cy.get("[data-cy='submit-btn']").contains("Submit").click();
+      });
+      cy.get("[data-cy='header-user-email']").should(
+        "contain",
+        "Meow! Welcome back journalist@mail.com"
       );
+      cy.get("[data-cy='article-form']").should("exist");
     });
-    cy.get("[data-cy='header-user-email']").contains(
-      "Woof! You're not logged in yet."
-    );
-    cy.get("[data-cy='article-form']").should("not.exist");
   });
-  it("sad path: unsuccessfully with right credentials but not an journalist", () => {
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/auth/sign_in",
-      response: "fixture:registered_user_can_not_log_in.json",
-    });
-    cy.route({
-      method: "GET",
-      url: "http://localhost:3000/api/auth/validate_token**",
-      response: "fixture:registered_user_can_not_log_in.json",
-    });
-    cy.visit("/");
-    cy.get("[data-cy='login-form']").within(() => {
-      cy.get("[data-cy='email']").type("registered@user.com");
-      cy.get("[data-cy='password']").type("password");
-      cy.get("[data-cy='submit-btn']").contains("Submit").click();
-      cy.get("[data-cy='error-message']").contains(
-        "You are not authorized to be here"
+  describe("unsuccessfully", () => {
+    it("with wrong credentials", () => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/auth/sign_in",
+        status: "401",
+        response: {
+          errors: ["Invalid login credentials. Please try again."],
+          success: false,
+        },
+      });
+      cy.get("[data-cy='login-form']").within(() => {
+        cy.get("[data-cy='email']").type("journalist@mail.com");
+        cy.get("[data-cy='password']").type("wrongpassword");
+        cy.get("[data-cy='submit-btn']").contains("Submit").click();
+        cy.get("[data-cy='error-message']").contains(
+          "Invalid login credentials. Please try again."
+        );
+      });
+      cy.get("[data-cy='header-user-email']").contains(
+        "Woof! You're not logged in yet."
       );
+      cy.get("[data-cy='article-form']").should("not.exist");
     });
-    cy.get("[data-cy='header-user-email']").contains(
-      "Woof! You're not logged in yet."
-    );
-    cy.get("[data-cy='article-form']").should("not.exist");
+
+    it("with right credentials but not an journalist", () => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/auth/sign_in",
+        response: "fixture:registered_user_can_not_log_in.json",
+      });
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/auth/validate_token**",
+        response: "fixture:registered_user_can_not_log_in.json",
+      });
+      cy.visit("/");
+      cy.get("[data-cy='login-form']").within(() => {
+        cy.get("[data-cy='email']").type("registered@user.com");
+        cy.get("[data-cy='password']").type("password");
+        cy.get("[data-cy='submit-btn']").contains("Submit").click();
+        cy.get("[data-cy='error-message']").contains(
+          "You are not authorized to be here"
+        );
+      });
+      cy.get("[data-cy='header-user-email']").contains(
+        "Woof! You're not logged in yet."
+      );
+      cy.get("[data-cy='article-form']").should("not.exist");
+    });
   });
 });

f21f20493d3072977750db746e3ce757643a065a
Ehugo2000 <erik.hgsn@gmail.com>
2021-01-17 19:50:08 +0100

    Adding favicon and Update page title.

--- src/components/CreateArticleForm.jsx
+++ src/components/CreateArticleForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   Button,
   Container,
@@ -9,57 +9,57 @@ import {
   Message,
   Divider,
   Item,
-} from "semantic-ui-react";
+} from 'semantic-ui-react'
 
-import { useDispatch, useSelector } from "react-redux";
-import ArticlesServices from "../modules/ArticlesServices";
+import { useDispatch, useSelector } from 'react-redux'
+import ArticlesServices from '../modules/ArticlesServices'
 
 const CreateArticleForm = () => {
-  const dispatch = useDispatch();
-  const { createArticleMessage, errorMessage } = useSelector((state) => state);
-  const [image, setImage] = useState();
-  const [title, setTitle] = useState();
-  const [lead, setLead] = useState();
-  const [body, setBody] = useState();
-  const [category, setCategory] = useState();
+  const dispatch = useDispatch()
+  const { createArticleMessage, errorMessage } = useSelector((state) => state)
+  const [image, setImage] = useState()
+  const [title, setTitle] = useState()
+  const [lead, setLead] = useState()
+  const [body, setBody] = useState()
+  const [category, setCategory] = useState()
 
   return (
     <Container>
       <Header>Create Article</Header>
       <Form
-        data-cy="article-form"
+        data-cy='article-form'
         onSubmit={(event) => ArticlesServices.create(event, dispatch)}
       >
         <Form.Field
-          data-cy="title-field"
-          label="Article title:"
+          data-cy='title-field'
+          label='Article title:'
           control={Input}
-          name="title"
-          placeholder="Title"
+          name='title'
+          placeholder='Title'
           onChange={(event) => setTitle(event.target.value)}
         />
         <Form.Field
-          data-cy="lead-field"
-          label="Article lead:"
+          data-cy='lead-field'
+          label='Article lead:'
           control={Input}
-          name="lead"
-          placeholder="Lead"
+          name='lead'
+          placeholder='Lead'
           onChange={(event) => setLead(event.target.value)}
         />
         <Form.Field
-          data-cy="body-field"
-          label="Article body:"
+          data-cy='body-field'
+          label='Article body:'
           control={TextArea}
-          name="body"
-          placeholder="Body"
+          name='body'
+          placeholder='Body'
           onChange={(event) => setBody(event.target.value)}
         />
         <Form.Field>
-          <label for="categories">Choose a category:</label>
+          <label for='categories'>Choose a category:</label>
           <select
-            name="categories"
-            id="categories"
-            data-cy="categories-dropdown"
+            name='categories'
+            id='categories'
+            data-cy='categories-dropdown'
             onChange={(event) =>
               setCategory(event.target[event.target.value].innerText)
             }
@@ -73,58 +73,58 @@ const CreateArticleForm = () => {
           </select>
         </Form.Field>
         <Form.Input
-          name="file_input"
-          placeholder="Image"
-          type="file"
-          label="Image:"
-          data-cy="file-input"
+          name='file_input'
+          placeholder='Image'
+          type='file'
+          label='Image:'
+          data-cy='file-input'
           onChange={(event) => setImage(event.target.files[0])}
         />
         <Button
-          data-cy="create-article-button"
-          type="submit"
-          color="teal"
-          value="submit"
+          data-cy='create-article-button'
+          type='submit'
+          color='teal'
+          value='submit'
         >
           Create Article
         </Button>
         {createArticleMessage && (
           <Message
-            color="green"
-            size="big"
-            data-cy="api-response-success-message"
+            color='green'
+            size='big'
+            data-cy='api-response-success-message'
           >
             {createArticleMessage}
           </Message>
         )}
         {errorMessage && (
-          <Message color="red" size="big" data-cy="api-response-error-message">
+          <Message color='red' size='big' data-cy='api-response-error-message'>
             {errorMessage}
           </Message>
         )}
       </Form>
       <Divider horizontal>Article Preview</Divider>
       <Item.Group>
-        <Item data-cy="preview-article-item">
+        <Item data-cy='preview-article-item'>
           {image && (
-            <Item.Image size="small" src={URL.createObjectURL(image)} />
+            <Item.Image size='small' src={URL.createObjectURL(image)} />
           )}
           <Item.Content>
-            <Item.Header data-cy="preview-title">Title: {title}</Item.Header>
-            <Item.Description data-cy="preview-lead">
+            <Item.Header data-cy='preview-title'>Title: {title}</Item.Header>
+            <Item.Description data-cy='preview-lead'>
               Lead: {lead}
             </Item.Description>
-            <Item.Description data-cy="preview-body">
+            <Item.Description data-cy='preview-body'>
               Body: {body}
             </Item.Description>
-            <Item.Description data-cy="preview-category">
+            <Item.Description data-cy='preview-category'>
               Category: {category}
             </Item.Description>
           </Item.Content>
         </Item>
       </Item.Group>
     </Container>
-  );
-};
+  )
+}
 
-export default CreateArticleForm;
+export default CreateArticleForm
